7a026b7585c9919f2d2b4a181364451738138ba7
KyawZinLin3 <kyawzin.lin@kbzbank.com>
2024-02-13 19:02:00 +0630

    learning

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,10 +1,13 @@
 import Starter from "../src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
 import Final from "../src/tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data";
+
+import NavBar from "../src/tutorial/04-project-structure/starter/Navbar";
 function App() {
   return (
     <div className="container">
-      <Starter />
+      {/* <Starter /> */}
       {/* <Final /> */}
+      <NavBar />
     </div>
   );
 }
